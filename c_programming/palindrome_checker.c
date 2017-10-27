9ee446fb8ccc2c9910a2ad2a37f5a66442b2e9d6
AjayJayendran <ajayjayendran.gmx@gmail.com>
2017-10-27 14:48:21 +0530

    Added palindrome checker in c

---
+++ c_programming/palindrome_checker.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+ 
+// A function to check if a string str is palindrome
+void isPalindrome(char str[])
+{
+    // Start from leftmost and rightmost corners of str
+    int l = 0;
+    int h = strlen(str) - 1;
+ 
+    // Keep comparing characters while they are same
+    while (h > l)
+    {
+        if (str[l++] != str[h--])
+        {
+            printf("%s is Not Palindromen", str);
+            return;
+        }
+    }
+    printf("%s is palindromen", str);
+}
+
+int main()
+{
+    isPalindrome("abba");
+    isPalindrome("abbccbba");
+    isPalindrome("hacktoberfest2k17");
+    return 0;
+}
